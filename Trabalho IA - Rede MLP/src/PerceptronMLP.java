27f77fb0864c4b867f5002e61f2bc31b61964b0e
AndersonKipper <kipper.and@gmail.com>
2017-04-26 22:21:42 -0300

    adicionado codigo para teste.

--- Trabalho IA - Rede MLP/src/PerceptronMLP.java
+++ Trabalho IA - Rede MLP/src/PerceptronMLP.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectInputStream.GetField;
+import java.util.Scanner;
 
 public class PerceptronMLP {
 
@@ -57,7 +58,7 @@ public class PerceptronMLP {
 			
 			br = new BufferedReader(new FileReader(arquivoCSV));
 			br.readLine();
-			int nlinha = 0;
+			//int nlinha = 0;
 			while ((linha = br.readLine()) != null) {
 				//System.out.println("\n\nLinha: " + nlinha++);
 				
@@ -199,13 +200,125 @@ public class PerceptronMLP {
 			}
 		}
 		
+		while(true){
+			
+				
+				String[] entradas = new String [totalEntradas];
+				double[] x = new double[entradas.length];
+				double y[] = new double[neuronioCamOcult.length];
+				int ySaida[] = new int[neuronioSaida.length];
+				int erro[] = new int[neuronioSaida.length];
+				int d[] = new int[neuronioSaida.length];
+				double gradienteSaida[] = new double[neuronioSaida.length];
+				double gradienteOculta[] = new double[neuronioCamOcult.length];
+				double ajusteW[] = new double[neuronioCamOcult.length];
+				boolean sair = false;
+				
+				Scanner dados = new Scanner(System.in);
+				
+				System.out.println("\n\nDigite -100 para encerrar");
+				for(int i = 1; i < x.length; i++){
+					System.out.println("Digite X" + i + ": ");
+					x[i] = dados.nextDouble();
+					
+					if(x[i] == -100){
+						sair = true;
+						break;
+					}
+				}
+				 if(sair == true) break;
+
+					for (int n = 0; n <  neuronioCamOcult.length ; n++) {
+
+						x[0] = 1;
+
+						for (int i = 0; i < x.length; i++) {
+
+							double c1 = (x[i] - 0) / (67 - x[i]/0.076) ; // inventei
+
+							x[i] = (c1);// normalizar
+																			
+
+						}
+						
+						y[n] = neuronioCamOcult[n].calculaY(x);
+						// neuronioCamOcult[n].setW(entrada, i + 1);
+					}
+					// Camada saida
+					for (int n2 = 0; n2 < neuronioSaida.length; n2++) {
+						//System.out.print("\n - Neuronio Saida");
+						ySaida[n2] = neuronioSaida[n2].calculaY(y);
+						// System.out.print("ysaida: " + ySaida[n2] + "\n");
+
+						// fazer um IF testando retorno ySaida com binario
+						// Os 4 precisam estaar corretos para passar? o if deve
+						// ter os 4 ou um de cada vez?
+						if (ySaida[n2] != d[n2]) {
+
+							
+
+							// System.out.print(getBinarioDecimal(saidaBinaria));
+
+							erro[n2] = d[n2] - ySaida[n2]; // ver se eh double
+
+							// DESCOBRIR como calcular o gradiente
+							gradienteSaida[n2] = neuronioSaida[n2]
+									.calculaGradienteSaida(ySaida[n2], erro[n2]);
+
+							// ajustar os pesos
+
+							double deltaW[] = new double[neuronioCamOcult.length];
+
+							for (int w = 0, g = 0; w < neuronioCamOcult.length; w++, g++) {
+								// delta
+								deltaW[w] = gradienteSaida[g] * eta;
+
+								// ajuste
+								double oldW = neuronioSaida[n2].getW(w);
+								ajusteW[n2] = oldW + deltaW[w];
+								neuronioSaida[n2].setW(ajusteW[n2], w);
+
+								/*System.out.printf("\nPeso novo W[" + w + "] = "
+										+ neuronioSaida[n2].getW(w) + "\n");*/
+								if (g == neuronioSaida.length - 1)
+									g = 0;
+							}
+
+						}
+					}
+
+					// calcular gradiente camada oculta
+
+					double deltaW[] = new double[neuronioCamOcult.length];
+					for (int n = 0; n < neuronioCamOcult.length; n++) {
+
+						gradienteOculta[n] = neuronioCamOcult[n]
+								.calculaGradienteOculta(x[n], gradienteSaida,
+										ajusteW);
+
+						deltaW[n] = gradienteOculta[n] * eta * x[n];
+
+						neuronioCamOcult[n].setW(neuronioCamOcult[n].getW(n)
+								+ deltaW[n], n);
+					}
+
+					String saida = "";
+					for(int i = 0; i < ySaida.length; i++)
+						saida += ySaida[i];
+					
+					System.out.printf("\n\nSaida = " + getBinarioDecimal(saida));
+					
+
+			}
+
+		 System.out.printf("\n\nPrograma Encerrado.");
 		
 
 	}
 
 	private static String getBinario(int num) {
 		String binario = Integer.toBinaryString(num).toString();
-		System.out.print(binario + " -- " + num);
+		//System.out.print(binario + " -- " + num);
 		return binario;
 	}
 
